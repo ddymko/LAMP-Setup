e2b889e1c7ef94394d1fbf950475a6a761fa206d
ddymko <dymkod@gmail.com>
2014-07-07 08:41:30 -0400

    added composer and laravel install

--- lamp.sh
+++ lamp.sh
@@ -18,3 +18,28 @@ sudo /usr/bin/mysql_secure_installation
 
 ##PHP install 
 sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-cli
+
+##Composer Installation
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+## laravel installation 
+wget http://laravel.com/laravel.phar
+sudo chmod 755 laravel.phar
+sudo mv laravel.phar /usr/local/bin/laravel
+
+
+##############
+#
+#
+#
+#
+#mv -i /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available/
+#sudo php5enmod mcrypt
+#sudo service apache2 restart
+#
+#
+#
+#
+#
+
